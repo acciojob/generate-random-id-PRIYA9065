455dd7551ff16ca02f4f77b19fefbb1a9da87659
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-09-05 01:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	let all = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	let id = "";
+	for(let i = 0; i < l; i++){
+		id = id + all[Math.floor(Math.random() * all.length)];
+	}
+	return id;
 }
 
 // Do not change the code below.
